3825c7311f7549349bda774928d226df28bbafed
Lucas Monzon <lucas.monzon@modo.com.ar>
2021-07-05 19:56:59 -0300

    fix

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,10 +3,14 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 
 const  PROMPT_MESSAGE_INGRESS_NAME='Ingrese su nombre'
-const  DEFAULT_VALUE_FOR_NAME='nombre'
+const  DEFAULT_VALUE_FOR_NAME=''
+const  PREFIX_MESSAGE_DISPLAY_NAME='Su nombre es '
+
 
 function mostrar()
-{   let nameEnteredByUser = prompt(PROMPT_MESSAGE_INGRESS_NAME,DEFAULT_VALUE_FOR_NAME);
-	alert(nameEnteredByUser);
+{   let nameEnteredByUser=DEFAULT_VALUE_FOR_NAME
+	nameEnteredByUser= prompt(PROMPT_MESSAGE_INGRESS_NAME,DEFAULT_VALUE_FOR_NAME);
+
+	alert(`${PREFIX_MESSAGE_DISPLAY_NAME} ${nameEnteredByUser}`);
 }
 
