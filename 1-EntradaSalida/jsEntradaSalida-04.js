2ee756e58bb3706fd7edaec67390efcf84bb6fc1
Lucas Monzon <lucas.monzon@modo.com.ar>
2021-07-05 19:56:41 -0300

    fix ejercicio 2

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -2,6 +2,10 @@
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
+
+const  PROMPT_MESSAGE_INGRESS_DATA='Ingrese el dato'
+const  DEFAULT_VALUE_FOR_NAME='nombre'
+
 function mostrar()
 {
 	alert("ok");
