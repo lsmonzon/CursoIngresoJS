e92890ac6996989a6f826fa276fd0525bd9a9f1d
Lucas Monzon <lucas.monzon@modo.com.ar>
2021-07-05 19:26:23 -0300

    resuelvo el ejerciocio

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,11 @@
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+
+//constant for the message to display
+const DISPLAY_MESSAGE = 'Esto funciona de maravilla'
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert(DISPLAY_MESSAGE);
 }
 
