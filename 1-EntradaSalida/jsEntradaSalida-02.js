8594108d11a832b195991fdde6f593dc75e75725
Lucas Monzon <lucas.monzon@modo.com.ar>
2021-07-05 19:36:28 -0300

    Ejercicio 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,12 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
+const  PROMPT_MESSAGE_INGRESS_NAME='Ingrese su nombre'
+const  DEFAULT_VALUE_FOR_NAME='nombre'
+
 function mostrar()
-{
-	alert("ok");
+{   let nameEnteredByUser = prompt(PROMPT_MESSAGE_INGRESS_NAME,DEFAULT_VALUE_FOR_NAME);
+	alert(nameEnteredByUser);
 }
 
